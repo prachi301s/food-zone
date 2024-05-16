fda9255311a5adb93625eaaf89a0b60bc512e3bc
prachi301s <prachisahu2711@gmail.com>
2024-05-16 16:36:33 +0530

    creating login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import { Grid, Paper, Typography } from "@mui/material";
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <Grid>
+        <Paper sx={{width:100,height:80}}>
+          <Typography textAlign="center">Login</Typography>
+        </Paper>
+
+      </Grid>
+    </>
+  );
+};
+
+export default Login;
