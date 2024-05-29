87ae949270626a4453323f0d8dc7672c317322d4
prachi301s <prachisahu2711@gmail.com>
2024-05-29 17:50:34 +0530

    changes added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
      
-   <Login/>
+   {/* <Login/> */}
+   <Home/>
     </>
   )
 }
